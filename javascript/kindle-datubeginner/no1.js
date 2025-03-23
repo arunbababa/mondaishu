9a5f81f86b887b2f278c6e7589fa1bd1e13f1cc2
arunbababa <hatuki.1.gzs@icloud.com>
2025-03-24 02:05:55 +0900

    Your commit message

---
+++ javascript/kindle-datubeginner/no1.js
@@ -0,0 +1,28 @@
+// 演習1（四則演算） 数字の2、3、10、10000を使って、以下の出力になるように四則演算をおこなってください。 
+// 10 + 3 = 13
+// 10 - 3 = 7
+// 10 × 3 = 30
+// 10000 ÷ 10 = 1000
+// 10 ÷ 3 = 3.3333333333333335 余り 1
+// 10 / 3 = 3 余り 1
+// 2の2乗 = 4
+// 2の3乗 = 8
+//  コードの最初はこのような感じです（他に方法があれば、それでも大丈夫です）。 let number_1 = 10;
+// let number_2 = 3;
+
+// フリーダイバージョージ. 脱初心者のための問題集 JavaScript編 (p.14). Kindle 版. 
+
+let num1 = 2;
+let num2 = 3;
+let num3 = 10;
+let num4 = 10000;
+
+console.log(num2 + num3); // 13
+console.log(num3 - num2); // 7
+console.log(num3 * num2); // 30
+console.log(num4 / num3); // 1000
+console.log(num3 / num2); // 3.3333333333333335 余り 1
+console.log(num3 % num2); // 1
+console.log(num1 ** 2); // 4
+console.log(num1 ** 3); // 8
+
---
+++ makefile
@@ -0,0 +1,5 @@
+# jsブランチ用のmakefile
+js:
+	git add .
+	git commit -m "Your commit message"
+	git push origin javascript
